7009d894a469c514ddc5b2c240f3c3f701250e0f
AmiltonCristian <amilton.cabral@ccc.ufcg.edu.br>
2022-01-03 09:07:34 -0300

    added another implementation of BuscaBinaria

---
+++ etapa_01_analise_de_algoritmo/BuscaBinaria2.java
@@ -0,0 +1,47 @@
+package etapa_01_analise_de_algoritmo;
+
+import java.util.Scanner;
+
+class BuscaBinaria2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] seq = converteLista(input.nextLine().split(" "));
+        int N = input.nextInt();
+        buscaBinaria(seq, N);
+        input.close();
+    }
+
+
+    public static int buscaBinaria(int[] seq, int N) {
+        return buscaBinaria(seq, N, 0, seq.length - 1);
+    }
+
+
+    private static int buscaBinaria(int[] seq, int N, int left, int right) {
+        int meio = (right + left) / 2;
+        System.out.println(meio);
+        if (seq[meio] == N) {
+            return seq[meio];
+        }
+
+        if (left < right) {
+            if (seq[meio] > N) {
+                return buscaBinaria(seq, N, left, meio - 1);
+            } else {
+                return buscaBinaria(seq, N, meio + 1, right);
+            }
+        }
+
+        System.out.println(-1);
+        return -1;
+    }
+
+
+    private static int[] converteLista(String[] lista) {
+        int[] novaLista = new int[lista.length];
+        for (int i=0; i<lista.length; i++) {
+            novaLista[i] = Integer.parseInt(lista[i]);
+        }
+        return novaLista;
+    }
+}
