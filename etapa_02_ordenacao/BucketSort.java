9cc709488a587c227354c08a9f8fb51af391e3a6
AmiltonCristian <amilton.cabral@ccc.ufcg.edu.br>
2022-02-10 10:15:40 -0300

    to do

---
+++ etapa_02_ordenacao/BucketSort.java
@@ -0,0 +1,13 @@
+package etapa_02_ordenacao;
+
+/**
+ * BucketSort
+ * 
+ * Será que é possível criar um bucketSort que varia a quantidade de bucket de acordo
+ * com o array pra sempre buscar o melhor "configuração" para cada array. O tornando 
+ * eficiente para diversas variedades?
+ */
+public class BucketSort {
+
+    
+}
