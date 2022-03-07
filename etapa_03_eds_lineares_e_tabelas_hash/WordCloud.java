d8dd2b87670dc70df38f5d57e598797c2d190bb4
AmiltonCristian <amilton.cabral@ccc.ufcg.edu.br>
2022-03-07 18:33:55 -0300

    added wordCloud

---
+++ etapa_03_eds_lineares_e_tabelas_hash/WordCloud.java
@@ -0,0 +1,32 @@
+package etapa_03_eds_lineares_e_tabelas_hash;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * WordCloud
+ */
+public class WordCloud {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashMap freq = new HashMap<>();
+        String[] frase = input.nextLine().split(" ");
+        for(String palavra : frase) {
+            if(freq.containsKey(palavra)) {
+                freq.put(palavra, ((int) freq.get(palavra) + 1));
+            } else {
+                freq.put(palavra, 1);
+            }
+        }
+
+        while (true) {
+            String entry = input.nextLine();
+            if (entry.equals("fim")) break;
+
+            System.out.println(freq.get(entry));
+        }
+
+        input.close();
+    }
+}
